665ce76e1d4c5c86a6f4cee4d50ca676005f80e8
Elias Boutaleb <e.boutaleb@hotmail.fr>
2016-04-06 13:22:55 +0200

    Refactoring

--- libs/ocplib-dwarf/dwarfDIE.ml
+++ libs/ocplib-dwarf/dwarfDIE.ml
@@ -37,7 +37,7 @@ type dwarf_DIE =
       die_ofs : int;
       die_cu_header : dwarf_DIE_header option;
       die_parent : dwarf_DIE option;
-      die_children : dwarf_DIE list;
+      mutable die_children : dwarf_DIE list;
       die_tag          : dwarf_TAG;
       die_attributes   : (dwarf_AT * Form.dwarf_FORM) list;
       die_attribute_vals : (Class.form_class * Form_data.form_data) list;
@@ -69,17 +69,36 @@ let readAllDIE abbrev_tbl s =
             Some(Hashtbl.find tbl code)
         with Not_found -> None in
 
+    let read_DIE_header s =
+        let (dwarf_format, initial_length) = get_initial_length s in
+        let version = get_version s in
+        let abbrev_offset = get_abbrev_offset s dwarf_format in
+        let address_size = get_address_size s in
+
+        let initial_length_size = match dwarf_format with
+            DWF_32BITS -> 4
+            | DWF_64BITS -> 12 in
+
+        let abbrev_offset_size = match dwarf_format with
+            DWF_32BITS ->Printf.printf "dwarf format 32 bits\n"; 4
+            | DWF_64BITS -> Printf.printf "dwarf format 64 bits\n";8 in
+
+        {
+        format = dwarf_format;
+        unit_length = initial_length;
+        abbrev_offset = abbrev_offset;
+        address_size = address_size;
+        version = version;
+        initial_length_size = initial_length_size;
+        abbrev_offset_size = abbrev_offset_size;
+        } in
+
     let res = ref [] in
 
     let read_DIE_attrs d s =
-        DwarfPrinter.string_of_abbrev_decl d;
-        let vals = List.map (fun (n,f) -> Form.get_form s f) d.abbrev_attributes in
-        {empty_DIE with
-        die_attribute_vals = vals;
-        die_tag = d.abbrev_tag;
-        die_attributes = d.abbrev_attributes} in
+         List.map (fun (n,f) -> Form.get_form s f) d.abbrev_attributes in
 
-    let rec readADIE abtbl s lvl h =
+    let rec readADIE abtbl s lvl h parent_die =
 
           Printf.printf "now in level %d\n" lvl;
           let die_abbrev_code = read_uleb128 s in
@@ -87,53 +106,38 @@ let readAllDIE abbrev_tbl s =
           match get_abbrev_decl abtbl die_abbrev_code with
                         (*null DIEs are caught here*)
                         | None -> Printf.printf "abbrev decl for code %Ld not found\n" die_abbrev_code;
-                                  if lvl > 1 then readADIE abtbl s (lvl-1) None
+                                  if lvl > 1 then readADIE abtbl s (lvl-1) None None else
+                                      begin match parent_die with Some(d) -> d | None -> empty_DIE end
                         | Some(d) ->
+                                let vals = read_DIE_attrs d s in
+                                let cu = {empty_DIE with
+                                            die_attribute_vals = vals;
+                                            die_tag = d.abbrev_tag;
+                                            die_attributes = d.abbrev_attributes} in
 
                               begin
                               Printf.printf "abbrev decl for code %Ld found\n" die_abbrev_code;
-                                            let cu = read_DIE_attrs d s in
                                             if d.abbrev_has_children then begin
                                                 Printf.printf "<%d>going down lvl %d\n" lvl (lvl+1);
-                                                readADIE abtbl s (lvl+1) None
+                                                readADIE abtbl s (lvl+1) None None
                                             end
                                             else
-                                                if lvl == 0 then () else
-                                                readADIE abtbl s lvl None
+                                                if lvl == 0 then {cu with die_cu_header = h} else
+                                                readADIE abtbl s lvl None None
                                         end in
 
     while DwarfUtils.peek s != None do
 
-    let (dwarf_format, initial_length) = get_initial_length s in
-    let version = get_version s in
-    let abbrev_offset = get_abbrev_offset s dwarf_format in
-    let address_size = get_address_size s in
-
-    let initial_length_size = match dwarf_format with
-        DWF_32BITS -> 4
-        | DWF_64BITS -> 12 in
-
-    let abbrev_offset_size = match dwarf_format with
-        DWF_32BITS ->Printf.printf "dwarf format 32 bits\n"; 4
-        | DWF_64BITS -> Printf.printf "dwarf format 64 bits\n";8 in
-
-    let dw_DIE_CU_header = Some ({
-    format = dwarf_format;
-    unit_length = initial_length;
-    abbrev_offset = abbrev_offset;
-    address_size = address_size;
-    version = version;
-    initial_length_size = initial_length_size;
-    abbrev_offset_size = abbrev_offset_size;
-    }) in
+    let dw_DIE_CU_header = read_DIE_header s in
+    let abbrev_offset = dw_DIE_CU_header.abbrev_offset in
 
       begin
-
       match get_abbrev_tbl (Int64.to_int abbrev_offset) with
         | Some(curr_offset_tbl) -> begin
                                     Printf.printf "abbrev for offset 0x%Lx found\n" abbrev_offset;
-                                    readADIE curr_offset_tbl s 0 dw_DIE_CU_header
+                                    readADIE curr_offset_tbl s 0 (Some dw_DIE_CU_header) (Some empty_DIE); ()
                                     end
         | None -> Printf.printf "abbrev for offset %Lu not found\n" abbrev_offset;
-      end;
+      end
+
     done
